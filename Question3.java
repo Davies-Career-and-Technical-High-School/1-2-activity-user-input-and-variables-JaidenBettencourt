484caf711c37f300174c8daa681d8f1f7f29a01b
Jaiden Bettencourt <jaiden.bettencourt@daviestech.org>
2020-10-01 15:14:08 +0000

    finsihed

--- Question3.java
+++ Question3.java
@@ -1,11 +1,18 @@
 /* Activity 1.2 - Question 3 */
-
+import java.util.Scanner;
 
 
 class Question3 {
     public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
 
-        /* Write your code here */
+      String name;
+      String adjective;
 
+      System.out.println("what is your name?");
+      name = scan.nextLine();
+      System.out.println("What adjective describes you?");
+      adjective = scan.nextLine();
+      System.out.print("Your name is "+ name + " and you are "+ adjective);
     }
 }
